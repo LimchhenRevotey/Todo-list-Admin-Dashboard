e8d609e6e26e687f19c7d704f373358013704376
LimchhenRevotey <limchhenrevotey2004@email.com>
2026-01-21 12:25:21 +0700

    fix route and add dashboard page

--- src/router/index.js
+++ src/router/index.js
@@ -1,24 +1,47 @@
+import { createRouter, createWebHistory } from 'vue-router'
 import DasboardLayout from '@/layouts/DasboardLayout.vue'
 import Login from '@/views/auths/Login.vue'
 import Dashboard from '@/views/pages/dashboard/dashboard.vue'
 import UserDiretory from '@/views/pages/usersDiretory/User-Diretory.vue'
-import { createRouter, createWebHistory } from 'vue-router'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
-    { path: '/', name: 'Login', component: Login },
+    { path : '/', redirect: 'login'},
     {
-      path: '/', redirect: '/dashboard', name: 'Dasboard', component: DasboardLayout,
-      children: [
-        { path: '/dashboard', name: 'Dashboard', component: Dashboard },
-        { path: '/user', name: 'UserDiretory', component: UserDiretory}
-
-      ]
+      path: '/login',
+      name: 'Login',
+      component: Login,
+      meta:{
+        title: 'login'
+      }
     },
-
-
+    {
+      path : '/',
+      redirect: '/dashboard',
+      component: DasboardLayout,
+      children:[
+        { path: 'dashboard', name: 'Dashboard', component: Dashboard, meta: {title: 'dashboard'},},
+        { path: 'users', 
+          name: 'UserDiretory',
+          component: UserDiretory, 
+          meta: {title: 'users'},
+        }
+      ]
+    }
   ],
+  linkActiveClass: 'active'
 })
+// router.beforeEach((to, form, next) => {
+//   document.title = to.meta.title ? `${to.meta.title} | Todo-List` : 'Todo-List';
+//   let token =localStorage.getItem('token');
+//   if(!token && to.name !== 'Login'){
+//     next  ({name: 'Login'});
+//   }else if(token && to.name === 'Login'){
+//      next  ({name: 'Dashboard'});
+//   }else{
+//     next();
+//   }
+// })
 
 export default router
