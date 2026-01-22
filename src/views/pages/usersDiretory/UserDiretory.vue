de4634d77c381c591828498c39dc1caee28197d5
Vit Socheata <145731923+VitSocheata@users.noreply.github.com>
2026-01-22 10:30:34 +0700

    commit view user not already

--- src/views/pages/usersDiretory/UserDiretory.vue
+++ src/views/pages/usersDiretory/UserDiretory.vue
@@ -1,44 +1,55 @@
 <script setup>
-import { onMounted, ref } from "vue";
+import { ref, onMounted } from "vue";
 import BaseTable from "@/components/ui/BaseTable.vue";
 import api from "@/api/api";
+import { useRouter } from "vue-router";
+
+const router = useRouter();
 
 const users = ref([]);
 
-const token = ref(localStorage.getItem("token") || null);
-console.log(token.value);
 
-onMounted(()=>{
+/* Columns definition */
+const columns = [
+  {
+    key: "user",
+    label: "User Details",
+    class: "ps-4 text-start",
+    bodyClass: "ps-4 py-3 text-start",
+  },
+  { key: "role", label: "Role" },
+  { key: "status", label: "Account Status" },
+];
+
+onMounted(() => {
   getUsers();
-})
+});
 
-const getUsers = async () => {
-  try{
-    const res = await api.get('/users',{
-      headers : {
-        Authorization: `Bearer ${token.value}`,
-      }
-    });
-    users.value = res.data.data.items; 
-    console.log("Users:", users.value);
-  }
-  catch(error){
-    console.log(error);
+async function getUsers() {
+  try {
+    const res = await api.get("/users");
+    users.value = res.data.data.items;
+    console.log(users.value);
     
+  } catch (error) {
+    console.error(error);
   }
 }
 
 
-function viewUser(user) {
-  navigateToSection("view-user", user);
-}
+/* Actions */
+const viewUser = (user) => {
+  router.push({ name: "View-User", params: { id: user.id } });
+};
 
-function updateUser(user) {
-  navigateToSection("update-user", user);
-}
+const updateUser = (user) => {
+  router.push({ name: "UpdateUser", params: { id: user.id } });
+};
+
+
 
 function toggleStatus(user) {
-  user.active = !user.active;
+  user.status = !user.status;
 }
 </script>
 
@@ -46,12 +57,56 @@ function toggleStatus(user) {
   <section id="section-users" class="content-section">
     <div class="d-flex justify-content-between align-items-center mb-4">
       <h4 class="fw-bold mb-0">Users Directory</h4>
+
       <button class="btn btn-brand btn-sm d-flex align-items-center gap-2" onclick="navigateToSection('create-user')">
-        <user-plus size="20"></user-plus> Create New User
+        <user-plus size="20" />
+        Create New User
       </button>
     </div>
+
     <div class="card overflow-hidden">
-      <BaseTable :users="users" @view="viewUser" @edit="updateUser" @toggle-status="toggleStatus"></BaseTable>
+      <BaseTable :columns="columns" :rows="users">
+
+        <!-- User details -->
+        <template #cell-user="{ row }">
+          <div class="fw-bold">{{ row.fullname }}</div>
+          <div class="text-muted small">{{ row.email }}</div>
+        </template>
+
+        <!-- Role -->
+        <template #cell-role="{ row }">
+          <span class="badge" :class="row.role?.name.trim().toLowerCase() === 'admin'
+            ? 'bg-primary-subtle text-primary'
+            : 'bg-danger-subtle text-danger'">
+            {{ row.role?.name }}
+          </span>
+        </template>
+
+        <!-- Status -->
+        <template #cell-status="{ row }">
+          <span class="badge-status" :class="row.status
+            ? 'bg-success-subtle text-success'
+            : 'bg-danger-subtle text-danger'">
+            {{ row.status ? "ACTIVATED" : "DEACTIVATED" }}
+          </span>
+        </template>
+
+        <!-- Actions -->
+        <template #actions ="{ row }">
+          <button class="btn btn-sm action-view" title="View" @click="viewUser(row)">
+            <square-pen />
+          </button>
+
+          <button class="btn btn-sm action-edit ms-1" title="Edit" @click="updateUser(row)">
+            <eye />
+          </button>
+
+          <button class="btn btn-sm action-delete ms-1" title="Toggle Status" @click="toggleStatus(row)">
+            <trash />
+          </button>
+        </template>
+
+      </BaseTable>
     </div>
   </section>
 </template>
@@ -62,19 +117,6 @@ function toggleStatus(user) {
   border-radius: var(--card-radius);
   box-shadow: 0 4px 12px rgba(0, 0, 0, 0.02);
   background: #fff;
-  margin-bottom: 1.5rem;
-}
-
-.stat-card {
-  padding: 1.75rem;
-  border: none;
-  transition: all 0.2s;
-  cursor: pointer;
-}
-
-.stat-card:hover {
-  transform: translateY(-5px);
-  box-shadow: 0 8px 20px rgba(19, 112, 127, 0.08);
 }
 
 .btn-brand {
@@ -83,27 +125,25 @@ function toggleStatus(user) {
   padding: 0.6rem 1.5rem;
   border-radius: 0.75rem;
   font-weight: 700;
-  border: none;
-  transition: all 0.2s;
 }
 
-.btn-brand:hover {
-  background-color: var(--brand-hover);
-  color: white;
-}
-
-.table thead th {
-  background: #daeaeb;
-  font-size: 1rem;
-  text-transform: uppercase;
-  color: #13707f;
-  padding: 1.25rem 1rem;
-}
-
-.badge-status {
+.badge-status,
+.badge {
   padding: 0.4em 0.8em;
-  border-radius: 0.5rem;
+  border-radius: 2rem;
   font-weight: 700;
   font-size: 0.75rem;
+}
+
+.action-view {
+  color: #0d6dfd96;
+}
+
+.action-edit {
+  color: #1987549c;
+}
+
+.action-delete {
+  color: #dc3546c3;
 }
 </style>
